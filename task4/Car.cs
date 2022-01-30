9aa459ff1404739145d545685e8d01f142d422a9
asligadirova <aslisg@code.edu.az>
2022-01-30 23:04:30 +0400

    Add project files.

---
+++ task4/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    internal class Car
+    { 
+        
+        public string model;
+        public string brand;
+        public string color;
+        public int price;
+
+        public string GetInfo() 
+        {
+            string output = $"{model}\n" +
+                $"{brand}\n" +
+                $"{color}\n" +
+                $"{price}₼";
+
+            return output;
+        
+        
+        
+        
+        }
+
+    } 
+    
+    
+    
+    
+}
---
+++ task4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+
+            car1.brand = "Hyundai";
+            car1.model = "i30";
+            car1.color = "gray";
+            car1.price = 20900;
+
+            //Console.WriteLine(car1.GetInfo());
+           // Console.WriteLine("---------");
+            Car car2 = new Car();
+            car2.brand = "Fiat";
+            car2.model = "500C";
+            car2.color = "black";
+            car2.price = 16000;
+            //Console.WriteLine(car2.GetInfo());
+            //Console.WriteLine("-------");
+
+            Car[] cars = new Car[2];
+            cars[0] = car1; 
+            cars[1] = car2;
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.GetInfo());
+                
+            }
+            Console.WriteLine("--------");
+            Car c1 = cars[0];
+            Console.WriteLine(c1.GetInfo());
+
+        }
+    }
+}
